1ae660c78b4add7dc4be77f7d10fe8a4b61f131f
Wesley Marcelino <owmarcelino@gmail.com>
2022-03-14 21:35:34 -0300

    feat: hightlight cards integration

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -19,21 +19,33 @@ import {
   TransactionsList,
   LogoutButton,
 } from "./styles";
-import { DataListProps } from "./types";
+import { DataListProps, HightLightData, initialHightLightData } from "./types";
 import { collectionKey } from "../Register";
 
 export const Dashboard = (): JSX.Element => {
-  const [data, setData] = useState<DataListProps[]>([]);
+  const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [hightLightData, setHightLataData] = useState<HightLightData>(
+    initialHightLightData
+  );
 
   const loadTransactions = async () => {
     const response = await AsyncStorage.getItem(collectionKey);
 
     const transactions = response ? JSON.parse(response) : [];
 
+    let entriesTotal = 0;
+    let expensiveTotal = 0;
+
     console.log("transactions", transactions);
 
     const transactionsFormatted: DataListProps[] = transactions.map(
       (item: DataListProps) => {
+        if (item.type === "positive") {
+          entriesTotal += Number(item.amount);
+        } else if (item.type === "negative") {
+          expensiveTotal += Number(item.amount);
+        }
+
         const amount = Number(item.amount).toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
@@ -56,7 +68,30 @@ export const Dashboard = (): JSX.Element => {
       }
     );
 
-    setData(transactionsFormatted);
+    setHightLataData({
+      entries: {
+        total: entriesTotal.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        LastTransaction: undefined,
+      },
+      expensives: {
+        total: expensiveTotal.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        LastTransaction: undefined,
+      },
+      total: {
+        total: (entriesTotal - expensiveTotal).toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        LastTransaction: undefined,
+      },
+    });
+    setTransactions(transactionsFormatted);
   };
 
   /**
@@ -97,19 +132,19 @@ export const Dashboard = (): JSX.Element => {
       <HighlightCards>
         <HighlightCard
           title="Entradas"
-          amount="R$ 17.40,00"
+          amount={hightLightData.entries.total}
           lastTransaction="Última entrada dia 30 de Dezembro"
           type="up"
         />
         <HighlightCard
           title="Saidas"
-          amount="R$ 1.259,00"
+          amount={hightLightData.expensives.total}
           lastTransaction="Última saida dia 04 de Dezembro"
           type="down"
         />
         <HighlightCard
           title="Total"
-          amount="R$ 16.141,00"
+          amount={hightLightData.total.total}
           lastTransaction="01 à 30 de Dezembro"
           type="total"
         />
@@ -118,7 +153,7 @@ export const Dashboard = (): JSX.Element => {
       <Transactions>
         <Title> Listagem </Title>
         <TransactionsList
-          data={data}
+          data={transactions}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => <TransactionCard data={item} />}
         />
--- src/screens/Dashboard/types.ts
+++ src/screens/Dashboard/types.ts
@@ -3,3 +3,28 @@ import { TransactionCardData } from "../../components/TransactionCard/types";
 export interface DataListProps extends TransactionCardData {
   id: string;
 }
+
+export interface HightLightProps {
+  total: string;
+  LastTransaction: Date | undefined;
+}
+export interface HightLightData {
+  entries: HightLightProps;
+  expensives: HightLightProps;
+  total: HightLightProps;
+}
+
+export const initialHightLightData: HightLightData = {
+  entries: {
+    total: "0.00",
+    LastTransaction: undefined,
+  },
+  expensives: {
+    total: "0.00",
+    LastTransaction: undefined,
+  },
+  total: {
+    total: "0.00",
+    LastTransaction: undefined,
+  },
+};
